9bce27df6be72aab2ad958099f4a8f6be71d75b1
kohta-rainy <artgc4663@gmail.com>
2021-07-24 22:23:55 +0900

    ログイン、新規登録、ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :basic_auth
-
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
         username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date])
+    end
 end
