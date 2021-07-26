ccf38d3b540668dcff9611f042c3399426945567
kohta-rainy <artgc4663@gmail.com>
2021-07-26 12:25:30 +0900

    エラーテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :purchase_records
-  
-  validates :nickname ,       presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date,      presence: true
 
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name, presence: true
+    validates :first_name,      presence: true
+    validates :last_name_kana,  presence: true
+    validates :first_name_kana, presence: true
+  end
+
+  validates :birth_date, presence: true
+  validates :password, :password_confirmation, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
