6a6353ec61dc1593ecb3de9bde3022c7ba5ebd34
kohta-rainy <artgc4663@gmail.com>
2021-07-26 16:22:35 +0900

    ユーザー管理機能編集２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_date, :last_name, :first_name, :last_name_kana, :first_name_kana])
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -159,6 +159,18 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('First name kana is invalid')
     end
 
+    it 'last_name_kanaがカタカナ以外の全角文字だと登録できないこと' do
+      @user.last_name_kana = 'おおお'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Last name kana is invalid')
+    end
+
+    it 'first_name_kanaがカタカナ以外の全角文字だと登録できないこと' do
+      @user.first_name_kana = 'おおお'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name kana is invalid')
+    end
+
     it 'emailで「＠」がない場合、登録できない' do
       @user.email = 'aaacom'
       @user.valid?
