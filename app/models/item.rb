e30dec6944213bb2f89fee77479571ceb4f6b4b9
kohta-rainy <artgc4663@gmail.com>
2021-07-31 12:22:18 +0900

    修正６

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,6 @@ validates :name
 validates :info
 validates :image 
 validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-validates :price,format: { with: /\A[0-9]+\z/}
 end
 
 with_options numericality: { other_than:1 } do
