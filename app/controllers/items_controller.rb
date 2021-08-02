17ad10a05df0cc79551fee8775d26ea9962f47fa
kohta-rainy <artgc4663@gmail.com>
2021-08-02 14:05:23 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])   
+    redirect_to root_path unless current_user.id == @item.user_id 
+  end
+
+  def update  
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    if @item.update(item_params)
+      redirect_to item_path, notice: ''
+    else
+      render 'edit'
+    end
+  end
+
   private
   def item_params
    params.require(:item,).permit(:image,:name,:info,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id, :scheduled_delivery_id,:price).merge(user_id: current_user.id)
